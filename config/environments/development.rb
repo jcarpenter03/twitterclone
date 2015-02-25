cffe48d13273f588e9a96470b03f2adf6572ad50
James Carpenter <james.carpentera3@gmail.com>
2015-02-25 15:48:43 +0000

    added devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,6 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  
+  config.action_mailer.default_url_options = { host: 'confident-highgarden-420-197312.use1.nitrousbox.com', port: 3000 }
 end
